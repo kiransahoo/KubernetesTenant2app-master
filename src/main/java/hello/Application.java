5560297b534bc9e82ef1cc720e2f44ca5a0a36e6
kiran <kiransahoo@gmail.com>
2019-09-15 20:36:51 -0400

    Testing isolation

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,14 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        restTemplate = builder.build();
+        return restTemplate;
+    }
+
     @RequestMapping("/")
     public String home() {
-        return "Tenant 2- App";
+        return "Tenant 2- App" + "-->Repsonse from Tenant1" +
+                restTemplate.getForObject("http://ab65fdc6-tenantonenamespac-8ac8-343232585.us-west-2.elb.amazonaws.com",
+                        String.class);
     }
 
     public static void main(String[] args) {
